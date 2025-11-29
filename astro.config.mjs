d2f6569644b012ebf3ea180fd201e4edcd274389
ian4d <ianl4d@gmail.com>
2025-11-29 09:16:16 -0800

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ian4d.github.io',
-  base: '/p5js-void'
+  base: '/p5js-void',
 })
---
+++ src/env.d.ts
@@ -0,0 +1,3 @@
+interface Window {
+    autoInit: boolean = false;
+}
